dc830ec1a751c5da67e50a7220e249f8a1b8d34a
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2024-09-20 12:44:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+string[] processNames = { "notepad", "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Excel.lnk", "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Word.lnk" };
+Process[] processes = new Process[processNames.Length];
+ // Открываем приложения
+for (int i = 0; i < processNames.Length; i++)
+{
+    try
+    {
+        processes[i] = Process.Start(processNames[i]);
+        Console.WriteLine($"{processNames[i]} запущен с PID: {processes[i].Id}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+Console.WriteLine("Нажмите любую клавишу, чтобы завершить процессы...");
+Console.ReadKey(); // Отключение приложений по нажатию кнопки
+try
+{
+    foreach (Process process in processes)
+    {
+        process.Kill(); // Завершаем процессы
+    }
+}
+catch (Exception ex)
+{
+     Console.WriteLine(ex.Message);
+}
+
